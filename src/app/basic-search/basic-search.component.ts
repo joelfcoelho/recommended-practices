86205756dd283c1b87950b2b606bc52d2a427e96
Joel Coelho <joel.coelho@cognism.com>
2024-04-02 10:30:11 +0200

    move search operators to search component

--- src/app/basic-search/basic-search.component.ts
+++ src/app/basic-search/basic-search.component.ts
@@ -2,7 +2,7 @@ import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
-import { tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, tap } from 'rxjs';
 
 @Component({
   selector: 'app-basic-search',
@@ -18,7 +18,14 @@ export class BasicSearchComponent {
     validators: [Validators.required]
   });
 
-  onSignatureNameChange = toSignal(this.searchTerm.valueChanges.pipe(tap(output => this.newSearchTerm.emit(output))));
+  onSignatureNameChange = toSignal(
+    this.searchTerm.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      /** perform any other operations here */
+      tap(output => this.newSearchTerm.emit(output))
+    )
+  );
 
   @Output() newSearchTerm = new EventEmitter<string>();
 }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { TodoItem } from '../../main';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-todo-list',
@@ -15,18 +13,7 @@ export class TodoListComponent {
   todos = input.required<TodoItem[]>();
   filterText = input('');
 
-  debouncedFilter = toSignal(
-    toObservable(this.filterText).pipe(
-      debounceTime(300),
-      distinctUntilChanged()
-      /** perform any other operations here */
-    ),
-    {
-      initialValue: ''
-    }
-  );
-
   filteredTodos = computed(() =>
-    this.todos().filter(todo => todo.title.toLowerCase().includes(this.debouncedFilter().toLowerCase()))
+    this.todos().filter(todo => todo.title.toLowerCase().includes(this.filterText().toLowerCase()))
   );
 }
